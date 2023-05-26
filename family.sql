4116d6b6a5c931ca98172bd2f2742ed43e0a387c
AyaAli12 <ayaali1272001@gmail.com>
2023-05-26 10:39:48 +0300

    note:to run the project succefully you must put it in Darrebni folder and emport the database

--- family.sql
+++ family.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 25, 2023 at 07:24 AM
+-- Generation Time: May 26, 2023 at 09:37 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -35,8 +35,18 @@ CREATE TABLE `families` (
   `num_of_members` int(11) NOT NULL,
   `functional_state` varchar(50) NOT NULL,
   `phone` varchar(10) NOT NULL,
-  `adress` varchar(50) NOT NULL
+  `address` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `families`
+--
+
+INSERT INTO `families` (`id`, `fname`, `mname`, `lname`, `num_of_members`, `functional_state`, `phone`, `address`) VALUES
+(1, 'ali', 'ahmed', 'ali', 6, 'employee', '0988888888', 'center of city'),
+(4, 'ali', 'ahmed', 'ali', 6, 'employee', '0988888888', 'center of city'),
+(5, 'ali', 'mohammed', 'ali', 6, 'employee', '0988888888', 'center of city'),
+(6, 'ali', 'mohammed', 'ali', 7, 'employee', '0988888888', 'center of city');
 
 -- --------------------------------------------------------
 
@@ -47,8 +57,17 @@ CREATE TABLE `families` (
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `password` varchar(10) NOT NULL
+  `password` varchar(10) NOT NULL,
+  `type` varchar(50) NOT NULL DEFAULT 'employee'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `password`, `type`) VALUES
+(1, 'Aya', '12345678', 'admin'),
+(2, 'ali', '12345678', 'employee');
 
 --
 -- Indexes for dumped tables
@@ -74,13 +93,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `families`
 --
 ALTER TABLE `families`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
